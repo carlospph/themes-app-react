f7b05a6a87df9d16a911cf94c4e1ca94dd60dd47
carlospph <80932010+carlospph@users.noreply.github.com>
2025-03-27 07:54:24 -0300

    fazer um card App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edite <code>src/App.js</code> e salve para atualizar.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <div className="card">
+    <h4 className="title__card">
+      Tecnologia
+    </h4>
+    <p> É o uso adequado das melhores ferramentas disponível à época que estamos</p>
+    </>
+    </>
   );
 }
 
