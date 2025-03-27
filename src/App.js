d43fca66d8cbfd342db43375c863f783c58741f3
carlospph <80932010+carlospph@users.noreply.github.com>
2025-03-27 08:25:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-
+{/* import card from 'card.js'; */}
 function App() {
   return (
-      <>
-      <div className="card">
-    <h4 className="title__card">
-      Tecnologia
-    </h4>
-    <p> É o uso adequado das melhores ferramentas disponível à época que estamos</p>
+       <>
+
+    <section className="header">
+      <h1>Bem-vindo</h1>
+      <h2>Carlos Nascimento </h2>
+      <h3>PcD, formação acadêmica em administração</h3>
+      <p className="text__default">
+        Diversos cursos na área empresarial, experiência no setor público e privado, atuante no ambiente financeiro no uso de ferramentas de tecnologia.
+      </p>
+
+      <p className="text__default">
+        Fora do olhar e digitação na tela preta, gosta de fazer palavras cruzadas (diretas), ler, pesquisar e de ar puro. Admirador de versos e da escrita
+      </p>
+
+      </section>
+
     </>
-    </>
-  );
+   );
 }
 
 export default App;
